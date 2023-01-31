ee42e0c0907b7834c5a7eece10f3ce5103b6c8a6
root <fakhari.ali@gmail.com>
2023-01-31 23:27:07 +0330

    Token class just created

--- bestoon/web/models.py
+++ bestoon/web/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=128)
+    def __str__(self):
+        return "{}-{}".format(self.user,self.token)
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
--- bestoon/web/views.py
+++ bestoon/web/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse,JsonResponse
+from web.models import Token,Expense,Income,User
+from django.views.decorators.csrf import csrf_exempt
+from json import JSONEncoder
+from datetime import datetime
 
-
+csrf_exempt
 def submit_expense(request):
     return HttpResponse("hi")
 
 def blank_response(request):
-    return HttpResponse('you are looking at my blank page')
+    # Expense.objects.create(user="1", text="Parking", date = datetime.now(),amount="20000")
+    
+    return JsonResponse({
+    "status": "OK!",
+    # "Expense" : Expense.objects.filter(user=1)
+    })
+    # return HttpResponse('you are looking at my blank page')
